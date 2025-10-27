1b0739256be99210b0eb5f421eb26f579b339b48
Vito Secona <secona00@gmail.com>
2025-10-27 20:39:21 +0700

    feat(gc): add immix space

---
+++ vm/belvm_gc/src/immix/space.rs
@@ -0,0 +1,156 @@
+use std::{
+    collections::LinkedList,
+    ptr,
+    sync::Mutex,
+};
+
+pub const LOG_BYTES_IN_LINE: usize = 8;
+pub const LOG_BYTES_IN_BLOCK: usize = 16;
+
+pub const BYTES_IN_LINE: usize = 1 << LOG_BYTES_IN_LINE;
+pub const BYTES_IN_BLOCK: usize = 1 << LOG_BYTES_IN_BLOCK;
+pub const LINES_IN_BLOCK: usize = 1 << (LOG_BYTES_IN_BLOCK - LOG_BYTES_IN_LINE);
+
+#[derive(PartialEq, Eq, Debug, Copy, Clone)]
+pub enum LineMark {
+    Free,
+    Live,
+    FreshAlloc,
+    ConservLive,
+    PrevLive,
+}
+
+#[derive(PartialEq, Eq, Debug, Copy, Clone)]
+pub enum BlockMark {
+    Usable,
+    Full,
+}
+
+#[derive(Clone)]
+pub struct LineMarkTable {
+    space_start: ptr::NonNull<libc::c_void>,
+    ptr: *mut LineMark,
+    len: usize,
+}
+
+impl LineMarkTable {
+    pub fn new(space_start: ptr::NonNull<libc::c_void>, space_end: ptr::NonNull<libc::c_void>) -> Self {
+        let offset = unsafe { space_end.offset_from(space_start) };
+        let len = offset.unsigned_abs() / BYTES_IN_LINE;
+
+        let line_mark_table = unsafe {
+            let size: libc::size_t = std::mem::size_of::<LineMark>() * len;
+            libc::malloc(size) as *mut LineMark
+        };
+
+        let mut cursor = line_mark_table;
+        for _ in 0..len {
+            unsafe { *cursor = LineMark::Free };
+            cursor = unsafe { cursor.add(1) };
+        }
+
+        Self {
+            space_start,
+            ptr: line_mark_table,
+            len,
+        }
+    }
+
+    pub fn take_slice(&mut self, start: usize, len: usize) -> LineMarkTableSlice {
+        let ptr = unsafe { self.ptr.offset(start as isize) };
+        LineMarkTableSlice { ptr, len }
+    }
+
+    #[inline(always)]
+    pub fn get(&self, index: usize) -> LineMark {
+        unsafe { *self.ptr.add(index) }
+    }
+}
+
+#[derive(Clone)]
+pub struct LineMarkTableSlice {
+    ptr: *mut LineMark,
+    len: usize,
+}
+
+impl LineMarkTableSlice {
+    #[inline(always)]
+    pub fn get(&self, index: usize) -> LineMark {
+        unsafe { *self.ptr.add(index) }
+    }
+}
+
+pub struct IxSpace {
+    mem_start: ptr::NonNull<libc::c_void>,
+    mem_end: ptr::NonNull<libc::c_void>,
+    blocks_usable: Mutex<LinkedList<Box<IxBlock>>>,
+    blocks_used: Mutex<LinkedList<Box<IxBlock>>>,
+    line_mark_table: LineMarkTable,
+}
+
+impl IxSpace {
+    pub fn new(len: usize) -> Self {
+        let addr = ptr::null_mut();
+        let prot = libc::PROT_READ | libc::PROT_WRITE;
+        let flags = libc::MAP_ANONYMOUS;
+        let fd = -1;
+        let offset = 0;
+
+        let (mem_start, mem_end) = unsafe {
+            let raw_ptr = libc::mmap(addr, len, prot, flags, fd, offset);
+            let start = ptr::NonNull::new_unchecked(raw_ptr);
+            let end = start.add(len);
+            (start, end)
+        };
+
+        let line_mark_table = LineMarkTable::new(mem_start, mem_end);
+
+        let mut space = Self {
+            mem_start,
+            mem_end,
+            blocks_usable: Mutex::new(LinkedList::new()),
+            blocks_used: Mutex::new(LinkedList::new()),
+            line_mark_table,
+        };
+
+        let mut blocks_usable = space.blocks_usable.lock().unwrap();
+        let mut block_start = mem_start.clone();
+        let mut line = 0;
+        while unsafe { block_start.add(BYTES_IN_BLOCK) } <= mem_end {
+            let block = IxBlock {
+                state: BlockMark::Usable,
+                start: block_start,
+                line_mark_table: space.line_mark_table.take_slice(line, LINES_IN_BLOCK),
+            };
+            blocks_usable.push_back(Box::new(block));
+            block_start = unsafe { block_start.add(BYTES_IN_BLOCK) };
+            line += LINES_IN_BLOCK;
+        }
+        drop(blocks_usable);
+
+        space
+    }
+
+    pub fn return_used_block(&self, old: Box<IxBlock>) {
+        self.blocks_used.lock().unwrap().push_front(old);
+    }
+}
+
+pub struct IxBlock {
+    state: BlockMark,
+    start: ptr::NonNull<libc::c_void>,
+    line_mark_table: LineMarkTableSlice,
+}
+
+impl IxBlock {
+    pub fn get_next_available_line(&self, curr_line: usize) -> Option<usize> {
+        let mut i = curr_line;
+        while i < self.line_mark_table.len {
+            if let LineMark::Free = self.line_mark_table.get(i) {
+                return Some(i);
+            }
+            i += 1;
+        }
+        None
+    }
+}
