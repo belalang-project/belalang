1edeb54717d1c971ab15edf6a680112b11f4486a
Vito Secona <secona00@gmail.com>
2025-11-20 23:14:39 +0700

    refactor: separate memory allocation

--- vm/belvm_gc/src/space.rs
+++ vm/belvm_gc/src/space.rs
@@ -100,29 +100,35 @@ pub struct IxSpace {
 unsafe impl Send for IxSpace {}
 unsafe impl Sync for IxSpace {}
 
+#[cfg(unix)]
+unsafe fn alloc_space(len: usize) -> ptr::NonNull<libc::c_void> {
+    let addr = ptr::null_mut();
+    let prot = libc::PROT_READ | libc::PROT_WRITE;
+    let flags = libc::MAP_ANONYMOUS | libc::MAP_PRIVATE;
+    let fd = -1;
+    let offset = 0;
+
+    let raw_ptr = unsafe { libc::mmap(addr, len, prot, flags, fd, offset) };
+
+    if raw_ptr == libc::MAP_FAILED {
+        panic!("mmap failed: {}", std::io::Error::last_os_error());
+    }
+
+    unsafe { ptr::NonNull::new_unchecked(raw_ptr) }
+}
+
+#[cfg(not(unix))]
+unsafe fn alloc_space(len: usize) -> ptr::NonNull<libc::c_void> {
+    ptr::NonNull::dangling()
+}
+
 impl IxSpace {
     pub fn new(len: usize) -> Self {
-        #[cfg(unix)]
         let (mem_start, mem_end) = unsafe {
-            let addr = ptr::null_mut();
-            let prot = libc::PROT_READ | libc::PROT_WRITE;
-            let flags = libc::MAP_ANONYMOUS | libc::MAP_PRIVATE;
-            let fd = -1;
-            let offset = 0;
-
-            let raw_ptr = libc::mmap(addr, len, prot, flags, fd, offset);
-
-            if raw_ptr == libc::MAP_FAILED {
-                panic!("mmap failed: {}", std::io::Error::last_os_error());
-            }
-
-            let start = ptr::NonNull::new_unchecked(raw_ptr);
+            let start = alloc_space(len);
             let end = start.add(len);
             (start, end)
         };
-
-        #[cfg(not(unix))]
-        let (mem_start, mem_end) = (ptr::NonNull::dangling(), ptr::NonNull::dangling());
 
         let line_mark_table = LineMarkTable::new(mem_start, mem_end);
 
