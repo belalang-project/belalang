7720bb73b090d2e71c85fad13456a5e19d378f29
Vito Secona <secona00@gmail.com>
2025-11-22 09:13:46 +0700

    feat: remove x86-specifics

--- vm/belvm_gc/src/gc.rs
+++ vm/belvm_gc/src/gc.rs
@@ -1,14 +1,11 @@
-use std::{
-    ptr,
-    sync::{
-        Arc,
-        OnceLock,
-        RwLock,
-        atomic::{
-            AtomicIsize,
-            AtomicUsize,
-            Ordering,
-        },
+use std::sync::{
+    Arc,
+    OnceLock,
+    RwLock,
+    atomic::{
+        AtomicIsize,
+        AtomicUsize,
+        Ordering,
     },
 };
 
@@ -52,63 +49,6 @@ pub fn trigger_gc() {
     }
 }
 
-#[cfg(target_arch = "x86_64")]
-pub fn stack_scan() -> Vec<ptr::NonNull<libc::c_void>> {
-    use crate::x86_64;
-
-    let stack_ptr = unsafe {
-        let sp_usize = x86_64::get_stack_ptr();
-        let sp_ptr = sp_usize as *mut libc::c_void;
-        ptr::NonNull::new_unchecked(sp_ptr)
-    };
-    let low_water_mark = unsafe {
-        let sp_usize = x86_64::get_low_water_mark();
-        let sp_ptr = sp_usize as *mut libc::c_void;
-        ptr::NonNull::new_unchecked(sp_ptr)
-    };
-
-    let mut cursor = stack_ptr;
-    let mut result = Vec::new();
-
-    let ctx = gc_context().read().unwrap();
-    let ix_space = ctx.immix_space.as_ref().unwrap();
-
-    while cursor < low_water_mark {
-        let value = cursor;
-
-        if is_valid_object(value, ix_space.start(), ix_space.end()) {
-            result.push(value);
-        }
-
-        cursor = unsafe { cursor.add(POINTER_SIZE) };
-    }
-
-    let reg_count = x86_64::get_registers_count();
-    let regs = unsafe { x86_64::get_registers() };
-
-    for i in 0..reg_count {
-        let value = unsafe { regs.offset(i as isize) };
-
-        if is_valid_object(value, ix_space.start(), ix_space.end()) {
-            result.push(value);
-        }
-    }
-
-    return result;
-
-    fn is_valid_object(
-        addr: ptr::NonNull<libc::c_void>,
-        start: ptr::NonNull<libc::c_void>,
-        end: ptr::NonNull<libc::c_void>,
-    ) -> bool {
-        if addr >= end || addr < start {
-            return false;
-        }
-
-        true
-    }
-}
-
 #[allow(unused_variables)]
 pub fn sync_barrier(mutator: &mut IxMutatorLocal) {
     let controller_id = CONTROLLER
@@ -116,10 +56,4 @@ pub fn sync_barrier(mutator: &mut IxMutatorLocal) {
         .unwrap();
 
     mutator.prepare_for_gc();
-
-    #[cfg(target_arch = "x86_64")]
-    {
-        let thread_roots = stack_scan();
-        todo!("Append thread_roots to roots");
-    }
 }
