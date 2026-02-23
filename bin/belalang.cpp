1b0c6408da299b39eb58470c34f4417a95bf38ac
Vito Secona <secona00@gmail.com>
2026-02-23 13:45:04 +0700

    feat: rewrite belalang_cli

--- bin/belalang.cpp
+++ bin/belalang.cpp
@@ -1,3 +1,26 @@
+#include <CLI/CLI.hpp>
 #include <iostream>
 
-int main() { std::cout << "Hello, belalang!\n"; }
+void setup_compile_command(CLI::App &app) {
+  auto *cmd = app.add_subcommand("compile", "compile source to bytecode");
+
+  cmd->callback([cmd] { std::cout << "Compiled!\n"; });
+}
+
+void setup_dis_command(CLI::App &app) {
+  auto *cmd = app.add_subcommand("dis", "disassemble bytecode to source");
+
+  cmd->callback([cmd] { std::cout << "Disassembled!\n"; });
+}
+
+int main(int argc, char **argv) {
+  CLI::App app{"Belalang"};
+
+  setup_compile_command(app);
+  setup_dis_command(app);
+
+  app.require_subcommand(1);
+
+  CLI11_PARSE(app, argc, argv);
+  return 0;
+}
