4efcba51dd1642c2bb169aa7c5356c952efd232c
Vito Secona <secona00@gmail.com>
2026-05-29 07:18:52 +0000

    feat: initial Bazel rewrite

---
+++ shell.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+(pkgs.buildFHSEnv {
+ name = "bazel";
+ targetPkgs = pkgs: [
+   pkgs.bazelisk
+   pkgs.glibc
+   pkgs.gcc
+   pkgs.zlib
+   pkgs.python313
+ ];
+}).env
