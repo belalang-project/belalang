256f3923e8a2e2ccd10888943a9c419e1dc411eb
Vito Secona <secona00@gmail.com>
2026-06-06 11:26:22 +0000

    feat: implement Builder for BIR

---
+++ bir/lib/IR/Builder.cpp
@@ -0,0 +1,88 @@
+#include "belalang/IR/Builder.h"
+#include "belalang/IR/BIRDialect.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/IR/Verifier.h"
+#include "llvm/Support/raw_ostream.h"
+
+namespace bir {
+
+BIRBuilder::BIRBuilder() : builder(&context), loc(builder.getUnknownLoc()) {
+  context.getOrLoadDialect<bir::BIRDialect>();
+  module = mlir::ModuleOp::create(loc);
+  builder.setInsertionPointToStart(module.getBody());
+}
+
+std::unique_ptr<BIRValue> BIRBuilder::build_constant_int(int64_t val) {
+  auto type = builder.getType<bir::IntType>();
+  auto op = bir::ConstantOp::create(builder, loc, type,
+                                    builder.getI32IntegerAttr(val));
+  return std::make_unique<BIRValue>(op.getResult());
+}
+
+std::unique_ptr<BIRValue> BIRBuilder::build_constant_float(double val) {
+  auto type = builder.getType<bir::FloatType>();
+  auto op =
+      bir::ConstantOp::create(builder, loc, type, builder.getF64FloatAttr(val));
+  return std::make_unique<BIRValue>(op.getResult());
+}
+
+std::unique_ptr<BIRValue> BIRBuilder::build_add(const BIRValue &lhs,
+                                                const BIRValue &rhs) {
+  auto type = lhs.getValue().getType();
+  auto op =
+      bir::AddOp::create(builder, loc, type, lhs.getValue(), rhs.getValue());
+  return std::make_unique<BIRValue>(op.getResult());
+}
+
+std::unique_ptr<BIRValue> BIRBuilder::build_sub(const BIRValue &lhs,
+                                                const BIRValue &rhs) {
+  auto type = lhs.getValue().getType();
+  auto op =
+      bir::SubOp::create(builder, loc, type, lhs.getValue(), rhs.getValue());
+  return std::make_unique<BIRValue>(op.getResult());
+}
+
+std::unique_ptr<BIRValue> BIRBuilder::build_mul(const BIRValue &lhs,
+                                                const BIRValue &rhs) {
+  auto type = lhs.getValue().getType();
+  auto op =
+      bir::MulOp::create(builder, loc, type, lhs.getValue(), rhs.getValue());
+  return std::make_unique<BIRValue>(op.getResult());
+}
+
+std::unique_ptr<BIRValue> BIRBuilder::build_div(const BIRValue &lhs,
+                                                const BIRValue &rhs) {
+  auto type = lhs.getValue().getType();
+  auto op =
+      bir::DivOp::create(builder, loc, type, lhs.getValue(), rhs.getValue());
+  return std::make_unique<BIRValue>(op.getResult());
+}
+
+std::unique_ptr<BIRValue> BIRBuilder::build_mod(const BIRValue &lhs,
+                                                const BIRValue &rhs) {
+  auto type = lhs.getValue().getType();
+  auto op =
+      bir::ModOp::create(builder, loc, type, lhs.getValue(), rhs.getValue());
+  return std::make_unique<BIRValue>(op.getResult());
+}
+
+void BIRBuilder::build_print(const BIRValue &val) {
+  bir::PrintOp::create(builder, loc, val.getValue());
+}
+
+void BIRBuilder::dump() const { const_cast<mlir::ModuleOp &>(module).dump(); }
+
+rust::String BIRBuilder::dump_to_string() const {
+  std::string s;
+  llvm::raw_string_ostream os(s);
+  const_cast<mlir::ModuleOp &>(module).print(os);
+  return rust::String(os.str());
+}
+
+std::unique_ptr<BIRBuilder> create_builder() {
+  return std::make_unique<BIRBuilder>();
+}
+
+} // namespace bir
--- bir/src/lib.rs
+++ bir/src/lib.rs
@@ -1,4 +1,43 @@
-#[cxx::bridge]
+#[cxx::bridge(namespace = "bir")]
 mod ffi {
-    unsafe extern "C++" {}
+    unsafe extern "C++" {
+        include!("belalang/IR/Builder.h");
+
+        type BIRValue;
+        type BIRBuilder;
+
+        fn create_builder() -> UniquePtr<BIRBuilder>;
+
+        fn build_constant_int(self: Pin<&mut BIRBuilder>, val: i64) -> UniquePtr<BIRValue>;
+        fn build_constant_float(self: Pin<&mut BIRBuilder>, val: f64) -> UniquePtr<BIRValue>;
+        fn build_add(self: Pin<&mut BIRBuilder>, lhs: &BIRValue, rhs: &BIRValue) -> UniquePtr<BIRValue>;
+        fn build_sub(self: Pin<&mut BIRBuilder>, lhs: &BIRValue, rhs: &BIRValue) -> UniquePtr<BIRValue>;
+        fn build_mul(self: Pin<&mut BIRBuilder>, lhs: &BIRValue, rhs: &BIRValue) -> UniquePtr<BIRValue>;
+        fn build_div(self: Pin<&mut BIRBuilder>, lhs: &BIRValue, rhs: &BIRValue) -> UniquePtr<BIRValue>;
+        fn build_mod(self: Pin<&mut BIRBuilder>, lhs: &BIRValue, rhs: &BIRValue) -> UniquePtr<BIRValue>;
+        fn build_print(self: Pin<&mut BIRBuilder>, val: &BIRValue);
+
+        fn dump(self: &BIRBuilder);
+        fn dump_to_string(self: &BIRBuilder) -> String;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_builder() {
+        let mut builder = ffi::create_builder();
+        let lhs = builder.pin_mut().build_constant_int(42);
+        let rhs = builder.pin_mut().build_constant_int(1);
+        let sum = builder.pin_mut().build_add(&lhs, &rhs);
+        builder.pin_mut().build_print(&sum);
+
+        let ir = builder.dump_to_string();
+        assert!(ir.contains("bir.constant 42"));
+        assert!(ir.contains("bir.constant 1"));
+        assert!(ir.contains("bir.add"));
+        assert!(ir.contains("bir.print"));
+    }
 }
