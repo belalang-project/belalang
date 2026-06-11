f7dc12d436094f6d1265d610b477c67395f2d46b
Vito Secona <77039267+secona@users.noreply.github.com>
2026-06-11 21:06:58 +0700

    [bir] implement CallOp (#115)

--- bir/lib/IR/BIROps.cpp
+++ bir/lib/IR/BIROps.cpp
@@ -5,7 +5,6 @@
 #include "mlir/IR/OpImplementation.h"
 #include "mlir/Interfaces/FunctionImplementation.h"
 #include "mlir/Support/LLVM.h"
-#include "llvm/ADT/TypeSwitch.h"
 
 #define GET_OP_CLASSES
 #include "belalang/BIR/IR/BIROps.cpp.inc"
@@ -59,6 +58,10 @@ void ConstantOp::print(mlir::OpAsmPrinter &p) {
   p.printOptionalAttrDict((*this)->getAttrs(), {"value"});
 }
 
+// -----------------------------------------------------------------------------
+// FuncOp
+// -----------------------------------------------------------------------------
+
 void FuncOp::build(mlir::OpBuilder &builder, mlir::OperationState &state,
                    llvm::StringRef name, mlir::FunctionType type,
                    llvm::ArrayRef<mlir::NamedAttribute> attrs) {
@@ -88,6 +91,100 @@ void FuncOp::print(mlir::OpAsmPrinter &p) {
   mlir::function_interface_impl::printFunctionOp(
       p, *this, false, getFunctionTypeAttrName(), getArgAttrsAttrName(),
       getResAttrsAttrName());
+}
+
+// -----------------------------------------------------------------------------
+// CallOp
+// -----------------------------------------------------------------------------
+
+void CallOp::build(mlir::OpBuilder &builder, mlir::OperationState &state,
+                   mlir::SymbolRefAttr callee, mlir::Type resType,
+                   mlir::ValueRange operands) {
+  state.addOperands(operands);
+  if (callee)
+    state.addAttribute("callee", callee);
+  if (resType)
+    state.addTypes(resType);
+}
+
+mlir::ParseResult CallOp::parse(mlir::OpAsmParser &parser,
+                                mlir::OperationState &result) {
+  mlir::FlatSymbolRefAttr calleeAttr;
+  llvm::SMLoc opsLoc;
+  llvm::SmallVector<mlir::OpAsmParser::UnresolvedOperand, 4> ops;
+
+  if (parser.parseAttribute(calleeAttr, "callee", result.attributes))
+    return mlir::failure();
+
+  opsLoc = parser.getCurrentLocation();
+  if (parser.parseOperandList(ops, mlir::AsmParser::Delimiter::Paren))
+    return mlir::failure();
+
+  if (parser.parseOptionalAttrDict(result.attributes))
+    return mlir::failure();
+
+  if (parser.parseColon())
+    return mlir::failure();
+
+  SmallVector<Type> argTypes;
+  SmallVector<DictionaryAttr> argAttrs;
+  SmallVector<Type> resultTypes;
+  SmallVector<DictionaryAttr> resultAttrs;
+
+  if (call_interface_impl::parseFunctionSignature(parser, argTypes, argAttrs,
+                                                  resultTypes, resultAttrs))
+    return mlir::failure();
+
+  result.addTypes(resultTypes);
+
+  if (parser.resolveOperands(ops, argTypes, opsLoc, result.operands))
+    return mlir::failure();
+
+  return mlir::success();
+}
+
+void CallOp::print(mlir::OpAsmPrinter &p) {
+  p << ' ';
+  p.printAttributeWithoutType(getCalleeAttr());
+  p << '(' << getArgOperands() << ')';
+  p << " : ";
+
+  call_interface_impl::printFunctionSignature(
+      p, getOperands().getTypes(), getArgAttrsAttr(), false,
+      (*this)->getResultTypes(), getResAttrsAttr());
+}
+
+// -----------------------------------------------------------------------------
+// CallOp: SymbolUserOpInterface
+// -----------------------------------------------------------------------------
+
+mlir::LogicalResult
+CallOp::verifySymbolUses(mlir::SymbolTableCollection &symbolTable) {
+  auto fnAttr = (*this)->getAttrOfType<FlatSymbolRefAttr>("callee");
+  auto fn = symbolTable.lookupNearestSymbolFrom<bir::FuncOp>(*this, fnAttr);
+
+  if (!fn)
+    return (*this)->emitOpError() << "'" << fnAttr.getValue()
+                                  << "' does not reference a valid function";
+
+  return mlir::success();
+}
+
+// -----------------------------------------------------------------------------
+// CallOp: CallOpInterface
+// -----------------------------------------------------------------------------
+
+mlir::OperandRange CallOp::getArgOperands() { return getArgs(); }
+mlir::MutableOperandRange CallOp::getArgOperandsMutable() {
+  return getArgsMutable();
+}
+
+mlir::CallInterfaceCallable CallOp::getCallableForCallee() {
+  return (*this)->getAttrOfType<mlir::SymbolRefAttr>("callee");
+}
+
+void CallOp::setCalleeFromCallable(mlir::CallInterfaceCallable callee) {
+  (*this)->setAttr("callee", mlir::cast<mlir::SymbolRefAttr>(callee));
 }
 
 } // namespace bir
