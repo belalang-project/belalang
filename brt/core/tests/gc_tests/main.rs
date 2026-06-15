b2cd17acb6060419ea9b5e6356ca946041826241
Vito Secona <secona00@gmail.com>
2026-06-15 08:19:11 +0000

    add mmtk test

--- brt/core/tests/gc_tests/main.rs
+++ brt/core/tests/gc_tests/main.rs
@@ -23,3 +23,16 @@ fn simple() {
 
     unsafe { ptr.write(t) };
 }
+
+#[test]
+fn mmtk_simple_alloc() {
+    brt_core::mmtk::init();
+
+    let ptr = brt_core::mmtk::alloc(64);
+    assert!(!ptr.is_null());
+
+    unsafe {
+        std::ptr::write(ptr.cast::<i32>(), 42);
+        assert_eq!(std::ptr::read(ptr.cast::<i32>()), 42);
+    }
+}
