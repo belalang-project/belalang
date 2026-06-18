253b904ae4d27ff7b61ee982145eb2f7b6e06932
Vito Secona <secona00@gmail.com>
2026-06-18 07:58:55 +0000

    define custom parsers

---
+++ bir/lib/IR/BIRAttrs.cpp
@@ -0,0 +1,51 @@
+#include "belalang/BIR/IR/BIR.h"
+
+#include "mlir/IR/DialectImplementation.h"
+
+namespace belalang {
+namespace bir {
+
+mlir::Attribute IntegerAttr::parse(mlir::AsmParser &p, mlir::Type attrType) {
+  if (p.parseLess())
+    return {};
+
+  uint64_t iVal;
+  if (p.parseInteger(iVal))
+    return {};
+  llvm::APInt value(64, iVal);
+
+  if (p.parseGreater())
+    return {};
+
+  return IntegerAttr::get(p.getContext(), attrType, value);
+}
+
+void IntegerAttr::print(mlir::AsmPrinter &p) const {
+  p << "<";
+  getValue().print(p.getStream(), true);
+  p << ">";
+}
+
+mlir::Attribute FloatAttr::parse(mlir::AsmParser &p, mlir::Type attrType) {
+  if (p.parseLess())
+    return {};
+
+  double fVal;
+  if (p.parseFloat(fVal))
+    return {};
+  llvm::APFloat value(fVal);
+
+  if (p.parseGreater())
+    return {};
+
+  return FloatAttr::get(p.getContext(), attrType, value);
+}
+
+void FloatAttr::print(mlir::AsmPrinter &p) const {
+  p << "<";
+  getValue().print(p.getStream());
+  p << ">";
+}
+
+} // namespace bir
+} // namespace belalang
--- bir/lib/IR/BIRDialect.cpp
+++ bir/lib/IR/BIRDialect.cpp
@@ -1,49 +1,6 @@
 #include "belalang/BIR/IR/BIR.h"
 
 #include "mlir/IR/DialectImplementation.h"
-#include "mlir/IR/OpImplementation.h"
-#include "mlir/IR/BuiltinTypes.h"
-#include "mlir/IR/DialectImplementation.h"
-
-#include "llvm/ADT/APFloat.h"
-
-static ParseResult parseIntLiteral(AsmParser &parser,
-                                   llvm::APInt &value,
-                                   Type type) {
-  int64_t val;
-  if (parser.parseInteger(val))
-    return parser.emitError(parser.getCurrentLocation(), "expected integer value");
-
-  value = llvm::APInt(64, val, true);
-  return success();
-}
-
-static void printIntLiteral(AsmPrinter &p,
-                            const llvm::APInt &value,
-                            Type type) {
-  p << value.getSExtValue();
-}
-
-static ParseResult parseFloatLiteral(AsmParser &p,
-                                   mlir::FailureOr<llvm::APFloat> &value,
-                                   Type type) {
-  auto floatType = llvm::dyn_cast<FloatType>(type);
-  if (!floatType)
-    return p.emitError(p.getCurrentLocation(), "expected a floating-point type");
-
-  llvm::APFloat floatVal(0.0);
-  if (p.parseFloat(floatType.getFloatSemantics(), floatVal))
-    return failure();
-
-  value.emplace(floatVal);
-  return success();
-}
-
-static void printFloatLiteral(AsmPrinter &p,
-                            const llvm::APFloat &value,
-                            Type type) {
-  p << value.convertToDouble();
-}
 
 #define GET_TYPEDEF_CLASSES
 #include "belalang/BIR/IR/BIRTypes.cpp.inc"
