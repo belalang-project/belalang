98b5c57063376ef3ae0aac4cfcac69b31896550d
Vito Secona <secona00@gmail.com>
2026-06-18 08:28:39 +0000

    ConstantOp parse attr

--- bir/lib/IR/BIROps.cpp
+++ bir/lib/IR/BIROps.cpp
@@ -17,45 +17,6 @@ namespace bir {
 // ConstantOp
 // -----------------------------------------------------------------------------
 
-mlir::ParseResult ConstantOp::parse(mlir::OpAsmParser &parser,
-                                    mlir::OperationState &result) {
-  int64_t iVal;
-  if (auto res = parser.parseOptionalInteger(iVal); res.has_value()) {
-    if (mlir::failed(*res))
-      return mlir::failure();
-    result.addAttribute("value", parser.getBuilder().getI32IntegerAttr(iVal));
-  } else {
-    double fVal;
-    if (mlir::succeeded(parser.parseFloat(fVal))) {
-      result.addAttribute("value", parser.getBuilder().getF32FloatAttr(fVal));
-    } else {
-      mlir::Attribute valueAttr;
-      if (parser.parseAttribute(valueAttr, "value", result.attributes))
-        return mlir::failure();
-    }
-  }
-
-  mlir::Type type;
-  if (parser.parseColonType(type))
-    return mlir::failure();
-
-  return parser.addTypeToList(type, result.types);
-}
-
-void ConstantOp::print(mlir::OpAsmPrinter &p) {
-  p << " ";
-  auto value = getValue();
-  if (auto intAttr = mlir::dyn_cast<mlir::IntegerAttr>(value)) {
-    p << intAttr.getInt();
-  } else if (auto floatAttr = mlir::dyn_cast<mlir::FloatAttr>(value)) {
-    p << floatAttr.getValueAsDouble();
-  } else {
-    p << value;
-  }
-  p << " : " << getType();
-  p.printOptionalAttrDict((*this)->getAttrs(), {"value"});
-}
-
 // -----------------------------------------------------------------------------
 // FuncOp
 // -----------------------------------------------------------------------------
