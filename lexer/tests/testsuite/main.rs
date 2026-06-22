8433a3fe57d5f5492c736f3796797d459a458a82
Vito Secona <secona00@gmail.com>
2026-06-22 05:30:51 +0000

    simple spans

--- lexer/tests/testsuite/main.rs
+++ lexer/tests/testsuite/main.rs
@@ -5,7 +5,10 @@ use lexer::{
     Token,
     TokenKind,
 };
-use session::Session;
+use session::{
+    Session,
+    SourceSpan,
+};
 
 #[track_caller]
 fn test_tokens(input: &str, expected: Vec<Token>) {
@@ -21,10 +24,19 @@ fn test_tokens(input: &str, expected: Vec<Token>) {
     assert_eq!(result, expected);
 }
 
-fn empty_token(kind: TokenKind) -> Token {
+fn token(kind: TokenKind, value: &str, start: usize, end: usize) -> Token {
+    Token {
+        kind,
+        value: value.to_string(),
+        span: SourceSpan::new(start, end),
+    }
+}
+
+fn empty_token(kind: TokenKind, start: usize, end: usize) -> Token {
     Token {
         kind,
         value: String::new(),
+        span: SourceSpan::new(start, end),
     }
 }
 
@@ -33,72 +45,108 @@ fn tokens_all() {
     test_tokens(
         "=+(){}[],;!-/*5;5 < 10 > 5;:= >= <= += -= /= %= *= || &&",
         vec![
-            empty_token(TokenKind::Assign {
-                kind: AssignmentKind::Assign,
-            }),
-            empty_token(TokenKind::Add),
-            empty_token(TokenKind::LeftParen),
-            empty_token(TokenKind::RightParen),
-            empty_token(TokenKind::LeftBrace),
-            empty_token(TokenKind::RightBrace),
-            empty_token(TokenKind::LeftBracket),
-            empty_token(TokenKind::RightBracket),
-            empty_token(TokenKind::Comma),
-            empty_token(TokenKind::Semicolon),
-            empty_token(TokenKind::Not),
-            empty_token(TokenKind::Sub),
-            empty_token(TokenKind::Div),
-            empty_token(TokenKind::Mul),
-            Token {
-                kind: TokenKind::Literal {
+            empty_token(
+                TokenKind::Assign {
+                    kind: AssignmentKind::Assign,
+                },
+                0,
+                1,
+            ),
+            empty_token(TokenKind::Add, 1, 2),
+            empty_token(TokenKind::LeftParen, 2, 3),
+            empty_token(TokenKind::RightParen, 3, 4),
+            empty_token(TokenKind::LeftBrace, 4, 5),
+            empty_token(TokenKind::RightBrace, 5, 6),
+            empty_token(TokenKind::LeftBracket, 6, 7),
+            empty_token(TokenKind::RightBracket, 7, 8),
+            empty_token(TokenKind::Comma, 8, 9),
+            empty_token(TokenKind::Semicolon, 9, 10),
+            empty_token(TokenKind::Not, 10, 11),
+            empty_token(TokenKind::Sub, 11, 12),
+            empty_token(TokenKind::Div, 12, 13),
+            empty_token(TokenKind::Mul, 13, 14),
+            token(
+                TokenKind::Literal {
                     kind: LiteralKind::Integer,
                 },
-                value: "5".into(),
-            },
-            empty_token(TokenKind::Semicolon),
-            Token {
-                kind: TokenKind::Literal {
+                "5",
+                14,
+                15,
+            ),
+            empty_token(TokenKind::Semicolon, 15, 16),
+            token(
+                TokenKind::Literal {
                     kind: LiteralKind::Integer,
                 },
-                value: "5".into(),
-            },
-            empty_token(TokenKind::Lt),
-            Token {
-                kind: TokenKind::Literal {
+                "5",
+                16,
+                17,
+            ),
+            empty_token(TokenKind::Lt, 18, 19),
+            token(
+                TokenKind::Literal {
                     kind: LiteralKind::Integer,
                 },
-                value: "10".into(),
-            },
-            empty_token(TokenKind::Gt),
-            Token {
-                kind: TokenKind::Literal {
+                "10",
+                20,
+                22,
+            ),
+            empty_token(TokenKind::Gt, 23, 24),
+            token(
+                TokenKind::Literal {
                     kind: LiteralKind::Integer,
                 },
-                value: "5".into(),
-            },
-            empty_token(TokenKind::Semicolon),
-            empty_token(TokenKind::Assign {
-                kind: AssignmentKind::ColonAssign,
-            }),
-            empty_token(TokenKind::Ge),
-            empty_token(TokenKind::Le),
-            empty_token(TokenKind::Assign {
-                kind: AssignmentKind::AddAssign,
-            }),
-            empty_token(TokenKind::Assign {
-                kind: AssignmentKind::SubAssign,
-            }),
-            empty_token(TokenKind::Assign {
-                kind: AssignmentKind::DivAssign,
-            }),
-            empty_token(TokenKind::Assign {
-                kind: AssignmentKind::ModAssign,
-            }),
-            empty_token(TokenKind::Assign {
-                kind: AssignmentKind::MulAssign,
-            }),
-            empty_token(TokenKind::Or),
-            empty_token(TokenKind::And),
+                "5",
+                25,
+                26,
+            ),
+            empty_token(TokenKind::Semicolon, 26, 27),
+            empty_token(
+                TokenKind::Assign {
+                    kind: AssignmentKind::ColonAssign,
+                },
+                27,
+                29,
+            ),
+            empty_token(TokenKind::Ge, 30, 32),
+            empty_token(TokenKind::Le, 33, 35),
+            empty_token(
+                TokenKind::Assign {
+                    kind: AssignmentKind::AddAssign,
+                },
+                36,
+                38,
+            ),
+            empty_token(
+                TokenKind::Assign {
+                    kind: AssignmentKind::SubAssign,
+                },
+                39,
+                41,
+            ),
+            empty_token(
+                TokenKind::Assign {
+                    kind: AssignmentKind::DivAssign,
+                },
+                42,
+                44,
+            ),
+            empty_token(
+                TokenKind::Assign {
+                    kind: AssignmentKind::ModAssign,
+                },
+                45,
+                47,
+            ),
+            empty_token(
+                TokenKind::Assign {
+                    kind: AssignmentKind::MulAssign,
+                },
+                48,
+                50,
+            ),
+            empty_token(TokenKind::Or, 51, 53),
+            empty_token(TokenKind::And, 54, 56),
         ],
     );
 }
@@ -108,19 +156,23 @@ fn tokens_strings() {
     test_tokens(
         r#""Hello, World"; "Test""#,
         vec![
-            Token {
-                kind: TokenKind::Literal {
+            token(
+                TokenKind::Literal {
                     kind: LiteralKind::String,
                 },
-                value: "Hello, World".into(),
-            },
-            empty_token(TokenKind::Semicolon),
-            Token {
-                kind: TokenKind::Literal {
+                "Hello, World",
+                0,
+                14,
+            ),
+            empty_token(TokenKind::Semicolon, 14, 15),
+            token(
+                TokenKind::Literal {
                     kind: LiteralKind::String,
                 },
-                value: "Test".into(),
-            },
+                "Test",
+                16,
+                22,
+            ),
         ],
     );
 }
@@ -130,33 +182,41 @@ fn tokens_integers() {
     test_tokens(
         "123; 456; 789 + 1",
         vec![
-            Token {
-                kind: TokenKind::Literal {
+            token(
+                TokenKind::Literal {
                     kind: LiteralKind::Integer,
                 },
-                value: "123".into(),
-            },
-            empty_token(TokenKind::Semicolon),
-            Token {
-                kind: TokenKind::Literal {
+                "123",
+                0,
+                3,
+            ),
+            empty_token(TokenKind::Semicolon, 3, 4),
+            token(
+                TokenKind::Literal {
                     kind: LiteralKind::Integer,
                 },
-                value: "456".into(),
-            },
-            empty_token(TokenKind::Semicolon),
-            Token {
-                kind: TokenKind::Literal {
+                "456",
+                5,
+                8,
+            ),
+            empty_token(TokenKind::Semicolon, 8, 9),
+            token(
+                TokenKind::Literal {
                     kind: LiteralKind::Integer,
                 },
-                value: "789".into(),
-            },
-            empty_token(TokenKind::Add),
-            Token {
-                kind: TokenKind::Literal {
+                "789",
+                10,
+                13,
+            ),
+            empty_token(TokenKind::Add, 14, 15),
+            token(
+                TokenKind::Literal {
                     kind: LiteralKind::Integer,
                 },
-                value: "1".into(),
-            },
+                "1",
+                16,
+                17,
+            ),
         ],
     );
 }
@@ -166,20 +226,11 @@ fn tokens_identifiers() {
     test_tokens(
         "x; x + y",
         vec![
-            Token {
-                kind: TokenKind::Ident,
-                value: "x".into(),
-            },
-            empty_token(TokenKind::Semicolon),
-            Token {
-                kind: TokenKind::Ident,
-                value: "x".into(),
-            },
-            empty_token(TokenKind::Add),
-            Token {
-                kind: TokenKind::Ident,
-                value: "y".into(),
-            },
+            token(TokenKind::Ident, "x", 0, 1),
+            empty_token(TokenKind::Semicolon, 1, 2),
+            token(TokenKind::Ident, "x", 3, 4),
+            empty_token(TokenKind::Add, 5, 6),
+            token(TokenKind::Ident, "y", 7, 8),
         ],
     );
 }
@@ -188,11 +239,13 @@ fn tokens_identifiers() {
 fn tokens_escape_strings() {
     test_tokens(
         r#""\n\r\t\"\x41""#,
-        vec![Token {
-            kind: TokenKind::Literal {
+        vec![token(
+            TokenKind::Literal {
                 kind: LiteralKind::String,
             },
-            value: "\n\r\t\"A".into(),
-        }],
+            "\n\r\t\"A",
+            0,
+            14,
+        )],
     );
 }
