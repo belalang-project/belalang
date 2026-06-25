5e2a4d1e555ead0ec0663d465dba0a7543fd36a2
Vito Secona <77039267+secona@users.noreply.github.com>
2026-06-25 08:59:55 +0700

    [chore][nix] Add flake.nix (#187)
    Replaces the shell.nix file the the new flake.nix.

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "The Belalang Programming Language";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+
+      perSystem =
+        {
+          config,
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          devShells.default = (pkgs.buildFHSEnv {
+            name = "bazel";
+            targetPkgs = pkgs: [
+              pkgs.bazelisk
+              pkgs.zlib
+              pkgs.python313
+              pkgs.clang-tools
+              pkgs.just
+            ];
+            profile = ''
+              export BRT_DIR="${toString ./.}/bazel-bin/brt"
+            '';
+          }).env;
+        };
+    };
+}
