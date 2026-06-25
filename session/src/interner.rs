9d1157251fc4ad0762a1a6fdc5821d10bd882704
Vito Secona <secona00@gmail.com>
2026-06-25 12:02:53 +0700

    session/Interner: populate keywords

--- session/src/interner.rs
+++ session/src/interner.rs
@@ -22,15 +22,45 @@ pub struct Interner {
 pub mod syms {
     use crate::interner::Symbol;
 
+    // Types
     pub const INT: Symbol = Symbol(0);
-    pub const PRINT: Symbol = Symbol(1);
+    pub const FLOAT: Symbol = Symbol(1);
+    pub const STRING: Symbol = Symbol(2);
+
+    // Keywords
+    pub const FN: Symbol = Symbol(3);
+    pub const WHILE: Symbol = Symbol(4);
+    pub const TRUE: Symbol = Symbol(5);
+    pub const FALSE: Symbol = Symbol(6);
+    pub const IF: Symbol = Symbol(7);
+    pub const ELSE: Symbol = Symbol(8);
+    pub const RETURN: Symbol = Symbol(9);
+
+    // Built-ins
+    pub const PRINT: Symbol = Symbol(10);
 }
 
 impl Interner {
     pub fn with_pre_interned_symbols() -> Self {
         let mut s = Self::default();
+
+        // Types
         s.intern("Int");
+        s.intern("Float");
+        s.intern("String");
+
+        // Keywords
+        s.intern("fn");
+        s.intern("while");
+        s.intern("true");
+        s.intern("false");
+        s.intern("if");
+        s.intern("else");
+        s.intern("return");
+
+        // Built-ins
         s.intern("print");
+
         s
     }
 
