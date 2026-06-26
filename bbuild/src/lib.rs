4b72ded9f0449933474616e250b71def314ed803
Vito Secona <77039267+secona@users.noreply.github.com>
2026-06-26 11:21:56 +0700

    [bbuild] Implement new `bbuild` component (#203)
    The bbuild component is essentially Belalang's build system. As of now,
    belalang only supports single file compilation. In the future when
    Belalang supports multi file compilation, this bbuild component will
    hopefully be the orchestrator to manage the build.
    
    Closes #201

---
+++ bbuild/src/lib.rs
@@ -0,0 +1,171 @@
+use std::{
+    env,
+    os::unix::process::CommandExt,
+    path::{
+        Path,
+        PathBuf,
+    },
+    process::Command,
+};
+
+use anyhow::Context;
+use ast::{
+    ASTDumper,
+    Parser,
+    Program,
+    TypeInferer,
+    Visitor,
+};
+use birgen::BIRGen;
+use lexer::Lexer;
+use session::Session;
+
+pub struct BuildContext {
+    pub use_color: bool,
+    pub emit: EmitTarget,
+}
+
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq)]
+pub enum EmitTarget {
+    Bir,
+    Ast,
+    Tokens,
+    Llvm,
+    Obj,
+    #[default]
+    Exe,
+}
+
+pub struct BBuild {
+    bctx: BuildContext,
+
+    cc: String,
+    brt_dir: String,
+
+    out_obj: PathBuf,
+    out_exe: PathBuf,
+
+    session: Session,
+}
+
+impl BBuild {
+    pub fn emit(&self) -> EmitTarget {
+        self.bctx.emit
+    }
+    pub fn new(source_path: &Path, bctx: BuildContext) -> anyhow::Result<Self> {
+        let cc = env::var("CC").unwrap_or("cc".to_string());
+        let brt_dir = env::var("BRT_DIR").unwrap_or_else(|_| "/usr/local/lib".to_string());
+
+        let out_obj = source_path.with_added_extension("o");
+        let out_exe = source_path.with_extension("");
+
+        let session = Session::for_file(source_path.to_path_buf())?;
+
+        Ok(Self {
+            cc,
+            brt_dir,
+            out_obj,
+            out_exe,
+            session,
+            bctx,
+        })
+    }
+
+    pub fn parse_program(&self) -> anyhow::Result<Program> {
+        let lexer = Lexer::new(&self.session);
+        let mut parser = Parser::new(&self.session, lexer);
+
+        let Ok(program) = parser.parse_program() else {
+            self.check_errors()?;
+            anyhow::bail!("compilation failed due to previous errors");
+        };
+        self.check_errors()?;
+
+        Ok(program)
+    }
+
+    pub fn dump_tokens(&self) -> anyhow::Result<()> {
+        let mut lexer = Lexer::new(&self.session);
+        let mut dumper = lexer::TokensDumper::new(&self.session, &mut lexer);
+        let res = dumper.dump();
+        self.check_errors()?;
+        res?;
+        Ok(())
+    }
+
+    pub fn infer_types(&self, program: &Program) -> anyhow::Result<()> {
+        let mut ty_infer = TypeInferer::new(&self.session);
+        ty_infer.infer(program);
+        self.check_errors()?;
+        Ok(())
+    }
+
+    pub fn dump_ast(&self, program: &Program) -> anyhow::Result<()> {
+        let mut dumper = ASTDumper::new(&self.session);
+        dumper.visit_program(program);
+        Ok(())
+    }
+
+    pub fn dump_bir(&self, program: &Program) -> String {
+        let mut birgen = BIRGen::new(&self.session);
+        birgen.generate_program(program);
+        birgen.optimize();
+        birgen.dump_to_string()
+    }
+
+    pub fn dump_llvm(&self, program: &Program) -> String {
+        let mut birgen = BIRGen::new(&self.session);
+        birgen.generate_program(program);
+        birgen.optimize();
+        let llvmgen = birgen.llvmgen();
+        llvmgen.dump_to_string()
+    }
+
+    pub fn compile_object_file(&self, program: &Program) -> anyhow::Result<String> {
+        let mut birgen = BIRGen::new(&self.session);
+        birgen.generate_program(program);
+        birgen.optimize();
+
+        let llvmgen = birgen.llvmgen();
+        let obj_out = self.out_obj.to_str().context("invalid UTF-8 data")?.to_string();
+        let compiled = llvmgen.compile_object_file(obj_out);
+
+        Ok(compiled)
+    }
+
+    pub fn link_objects(&self) -> anyhow::Result<()> {
+        let status = Command::new(&self.cc)
+            .arg(&self.out_obj)
+            .arg(format!("-L{}", self.brt_dir))
+            .arg("-lbrt")
+            .arg("-o")
+            .arg(&self.out_exe)
+            .status()?;
+
+        if !status.success() {
+            anyhow::bail!("linker failed with exit code: {}", status);
+        }
+
+        Ok(())
+    }
+
+    pub fn execute_artifact(&self) {
+        let exe = std::fs::canonicalize(&self.out_exe).unwrap();
+        let err = Command::new(exe).exec();
+    }
+
+    fn check_errors(&self) -> anyhow::Result<()> {
+        if self.session.has_errors() {
+            for d in self.session.take_diagnostics() {
+                diag::print_diagnostics(
+                    &self.session.source_text,
+                    self.session.get_source_file(),
+                    &d,
+                    self.bctx.use_color,
+                );
+            }
+            anyhow::bail!("compilation failed due to previous errors");
+        }
+        Ok(())
+    }
+}
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,29 +1,18 @@
 use std::{
-    env,
     io::{
         self,
         IsTerminal,
     },
-    os::unix::process::CommandExt,
     path::PathBuf,
 };
 
-use anyhow::Context;
-use ast::{
-    Parser,
-    TypeInferer,
-    Visitor,
-};
-use birgen::BIRGen;
 use clap::{
     Parser as ClapParser,
     Subcommand,
     ValueEnum,
 };
-use lexer::Lexer;
-use session::Session;
 
-#[derive(ValueEnum, Clone, Debug, Default)]
+#[derive(ValueEnum, Clone, Copy, Debug, Default)]
 enum EmitTarget {
     Bir,
     Ast,
@@ -44,34 +33,12 @@ enum ColorChoice {
 
 #[derive(clap::Args)]
 struct BuildArgs {
-    /// Path to the .bel file to compile
-    path: PathBuf,
-
     /// Path to the output file
     #[arg(long, short)]
     out: Option<PathBuf>,
 
-    /// What to emit
-    #[arg(long, value_enum, default_value_t = EmitTarget::Exe)]
-    emit: EmitTarget,
-}
-
-impl BuildArgs {
-    fn get_out_path(&self) -> Option<PathBuf> {
-        if let Some(out) = &self.out {
-            return Some(out.to_path_buf());
-        }
-
-        if let EmitTarget::Obj = self.emit {
-            return Some(self.path.with_added_extension("o"));
-        }
-
-        if let EmitTarget::Exe = self.emit {
-            return Some(self.path.with_added_extension("o"));
-        }
-
-        None
-    }
+    /// Path to the .bel file to compile
+    path: PathBuf,
 }
 
 #[derive(clap::Args)]
@@ -95,6 +62,10 @@ struct Belalang {
     /// Use color
     #[arg(long, value_enum, default_value_t = ColorChoice::Auto)]
     color: ColorChoice,
+
+    /// What to emit
+    #[arg(long, global = true, value_enum, default_value_t = EmitTarget::Exe)]
+    emit: EmitTarget,
 }
 
 impl Belalang {
@@ -107,143 +78,83 @@ impl Belalang {
     }
 }
 
-fn main() -> anyhow::Result<()> {
-    let belalang = Belalang::parse();
-    let use_color = belalang.use_color();
-
-    match belalang.command {
-        Commands::Build(args) => build(args, use_color),
-        Commands::Run(args) => run(args, use_color),
+impl From<EmitTarget> for bbuild::EmitTarget {
+    fn from(target: EmitTarget) -> Self {
+        match target {
+            EmitTarget::Bir => bbuild::EmitTarget::Bir,
+            EmitTarget::Ast => bbuild::EmitTarget::Ast,
+            EmitTarget::Tokens => bbuild::EmitTarget::Tokens,
+            EmitTarget::Llvm => bbuild::EmitTarget::Llvm,
+            EmitTarget::Obj => bbuild::EmitTarget::Obj,
+            EmitTarget::Exe => bbuild::EmitTarget::Exe,
+        }
     }
 }
 
-fn build(args: BuildArgs, use_color: bool) -> anyhow::Result<()> {
-    let session = Session::for_file(args.path.clone())?;
-
-    let mut lexer = Lexer::new(&session);
-
-    if let EmitTarget::Tokens = args.emit {
-        let mut dumper = lexer::TokensDumper::new(&session, &mut lexer);
-        let res = dumper.dump();
-        check_errors(&session, use_color)?;
-        res?;
-        return Ok(());
-    }
-
-    let mut parser = Parser::new(&session, lexer);
-    let Ok(program) = parser.parse_program() else {
-        check_errors(&session, use_color)?;
-        return Ok(());
+fn main() -> anyhow::Result<()> {
+    let belalang = Belalang::parse();
+    let (path, emit) = match &belalang.command {
+        Commands::Build(args) => (args.path.clone(), belalang.emit.into()),
+        Commands::Run(args) => (args.path.clone(), bbuild::EmitTarget::Exe),
     };
-    check_errors(&session, use_color)?;
 
-    let mut ty_infer = TypeInferer::new(&session);
-    ty_infer.infer(&program);
-    check_errors(&session, use_color)?;
+    let bctx = bbuild::BuildContext {
+        use_color: belalang.use_color(),
+        emit,
+    };
 
-    if let EmitTarget::Ast = args.emit {
-        let mut dumper = ast::ASTDumper::new(&session);
-        dumper.visit_program(&program);
+    let bb = bbuild::BBuild::new(&path, bctx)?;
+
+    match belalang.command {
+        Commands::Build(..) => build(bb),
+        Commands::Run(..) => run(bb),
+    }
+}
+
+fn build(bb: bbuild::BBuild) -> anyhow::Result<()> {
+    use bbuild::EmitTarget;
+
+    if let EmitTarget::Tokens = bb.emit() {
+        bb.dump_tokens()?;
         return Ok(());
     }
 
-    let mut birgen = BIRGen::new(&session);
-    birgen.generate_program(&program);
-    birgen.optimize();
+    let program = bb.parse_program()?;
+    bb.infer_types(&program)?;
 
-    if let EmitTarget::Bir = args.emit {
-        println!("{}", birgen.dump_to_string());
+    if let EmitTarget::Ast = bb.emit() {
+        bb.dump_ast(&program)?;
         return Ok(());
     }
 
-    let llvmgen = birgen.llvmgen();
-
-    if let EmitTarget::Llvm = args.emit {
-        println!("{}", llvmgen.dump_to_string());
+    if let EmitTarget::Bir = bb.emit() {
+        println!("{}", bb.dump_bir(&program));
         return Ok(());
     }
 
-    let out = args
-        .get_out_path()
-        .context("Path is None")?
-        .to_str()
-        .context("Path contains invalid UTF-8 data")?
-        .to_string();
-    println!("{}", llvmgen.compile_object_file(out.clone()));
-
-    if let EmitTarget::Obj = args.emit {
+    if let EmitTarget::Llvm = bb.emit() {
+        println!("{}", bb.dump_llvm(&program));
         return Ok(());
     }
 
-    let cc = env::var("CC").unwrap_or("cc".to_string());
-    let brt = env::var("BRT_DIR").unwrap_or_else(|_| "/usr/local/lib".to_string());
+    let compiled_msg = bb.compile_object_file(&program)?;
+    println!("{}", compiled_msg);
 
-    let status = std::process::Command::new(cc)
-        .arg(out)
-        .arg(format!("-L{}", brt))
-        .arg("-lbrt")
-        .arg("-o")
-        .arg(args.path.with_extension(""))
-        .status()?;
-
-    if !status.success() {
-        anyhow::bail!("linker failed with exit code: {}", status);
+    if let EmitTarget::Obj = bb.emit() {
+        return Ok(());
     }
+
+    bb.link_objects()?;
 
     Ok(())
 }
 
-fn run(args: RunArgs, use_color: bool) -> anyhow::Result<()> {
-    let session = Session::for_file(args.path.clone())?;
+fn run(bb: bbuild::BBuild) -> anyhow::Result<()> {
+    let program = bb.parse_program()?;
+    bb.infer_types(&program)?;
+    bb.compile_object_file(&program)?;
+    bb.link_objects()?;
+    bb.execute_artifact();
 
-    let lexer = Lexer::new(&session);
-
-    let mut parser = Parser::new(&session, lexer);
-    let Ok(program) = parser.parse_program() else {
-        check_errors(&session, use_color)?;
-        return Ok(());
-    };
-    check_errors(&session, use_color)?;
-
-    let mut birgen = BIRGen::new(&session);
-    birgen.generate_program(&program);
-    birgen.optimize();
-
-    let llvmgen = birgen.llvmgen();
-    let obj_out = args
-        .path
-        .with_added_extension("o")
-        .to_str()
-        .context("invalid UTF-8 data")?
-        .to_string();
-    let _ = llvmgen.compile_object_file(obj_out.clone());
-
-    let cc = env::var("CC").unwrap_or("cc".to_string());
-    let brt = env::var("BRT_DIR").unwrap_or_else(|_| "/usr/local/lib".to_string());
-
-    let status = std::process::Command::new(cc)
-        .arg(obj_out)
-        .arg(format!("-L{}", brt))
-        .arg("-lbrt")
-        .arg("-o")
-        .arg(args.path.with_extension(""))
-        .status()?;
-
-    if !status.success() {
-        anyhow::bail!("linker failed with exit code: {}", status);
-    }
-
-    let exe = std::fs::canonicalize(args.path.with_extension("")).context("Failed to canonicalize exe path")?;
-    let err = std::process::Command::new(exe).exec();
-    anyhow::bail!("Failed to exec: {}", err);
-}
-
-fn check_errors(session: &Session, use_color: bool) -> anyhow::Result<()> {
-    if session.has_errors() {
-        for d in session.take_diagnostics() {
-            diag::print_diagnostics(&session.source_text, session.get_source_file(), &d, use_color);
-        }
-        anyhow::bail!("compilation failed due to previous errors");
-    }
     Ok(())
 }
