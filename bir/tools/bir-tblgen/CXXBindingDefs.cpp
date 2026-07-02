086f62194e8da3eb0e4970214d754cb68405d6a9
Vito Secona <secona00@gmail.com>
2026-07-02 18:53:56 +0700

    split CXXBindingDecls and CXXBindingDefs

--- bir/tools/bir-tblgen/CXXBindingDefs.cpp
+++ bir/tools/bir-tblgen/CXXBindingDefs.cpp
@@ -1,38 +1,8 @@
-#include "belalang/BIR/IR/BIR.h"
-
 #include "TableGenBackends.h"
 #include "mlir/TableGen/Operator.h"
-#include "llvm/Support/FormatVariadic.h"
 #include "llvm/TableGen/TableGenBackend.h"
 
 namespace {
-
-std::vector<std::string> builderFunctions;
-
-const char *const BIRGenClass = R"(
-namespace belalang {{
-namespace birgen {{
-class BIRGen;
-} // namespace birgen
-} // namespace belalang
-
-namespace belalang::birgen2 {{
-
-class BIRGen2 {{
-public:
-  BIRGen2(::belalang::birgen::BIRGen &gen);
-  ~BIRGen2() = default;
-
-{0}
-
-private:
-  ::belalang::birgen::BIRGen &gen;
-};
-
-std::unique_ptr<BIRGen2> create_birgen2(uintptr_t gen);
-
-} // namespace belalang::birgen2
-)";
 
 std::string getArgs(mlir::tblgen::Operator op) {
   std::vector<std::string> args;
@@ -42,24 +12,6 @@ std::string getArgs(mlir::tblgen::Operator op) {
   }
 
   return llvm::join(args, ", ");
-}
-
-void genBuilderFunctionDecl(const llvm::Record *opRec, llvm::raw_ostream &os) {
-  mlir::tblgen::Operator op(opRec);
-
-  // TODO: support more
-  if (!(op.getNumResults() == 0 && op.getNumOperands() == 0 &&
-        op.getNumAttributes() == 0 && op.getNumRegions() == 0))
-    return;
-
-  llvm::StringRef name = op.getCppClassName();
-
-  std::string retTy =
-      op.getNumResults() > 0 ? "std::unique_ptr<BIRValue>" : "void";
-  auto args = getArgs(op);
-
-  std::string fn = "  " + retTy + " build" + name.str() + "(" + args + ");";
-  builderFunctions.push_back(fn);
 }
 
 void genBuilderFunctionDefs(const llvm::Record *opRec, llvm::raw_ostream &os) {
@@ -88,15 +40,9 @@ void genBuilderFunctionDefs(const llvm::Record *opRec, llvm::raw_ostream &os) {
   os << "}\n\n";
 }
 
-} // namespace
+}
 
 namespace belalang::bir {
-
-void emitCXXBindingsDecl(const llvm::RecordKeeper &rk, llvm::raw_ostream &os) {
-  for (const auto *op : rk.getAllDerivedDefinitions("BIR_Op"))
-    genBuilderFunctionDecl(op, os);
-  os << llvm::formatv(BIRGenClass, llvm::join(builderFunctions, "\n"));
-}
 
 void emitCXXBindingsDefs(const llvm::RecordKeeper &rk, llvm::raw_ostream &os) {
   os << "namespace belalang::birgen2 {\n";
