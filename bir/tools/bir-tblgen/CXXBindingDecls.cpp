556e3e8fce03b90dab3e1925a8d4dada8457c11d
Vito Secona <secona00@gmail.com>
2026-07-03 00:47:13 +0700

    clean up CXXBindingDecls

--- bir/tools/bir-tblgen/CXXBindingDecls.cpp
+++ bir/tools/bir-tblgen/CXXBindingDecls.cpp
@@ -8,54 +8,7 @@
 
 namespace {
 
-std::vector<std::string> builderFunctions;
-std::vector<std::string> guards;
-
-const char *const BIRGenClass = R"(
-class BIRGen2 {{
-public:
-  BIRGen2(::belalang::birgen::BIRGen &gen);
-  ~BIRGen2() = default;
-
-{0}
-
-private:
-  ::belalang::birgen::BIRGen &gen;
-};
-
-std::unique_ptr<BIRGen2> create_birgen2(uintptr_t gen);
-)";
-
-const char *const BIRGuardClass = R"(
-class BIRGuard {
-public:
-  BIRGuard(mlir::OpBuilder &builder) : guard(builder), builder(builder) {}
-  virtual ~BIRGuard() = default;
-
-protected:
-  mlir::OpBuilder::InsertionGuard guard;
-  mlir::OpBuilder &builder;
-};
-)";
-
-// 0: op name
-// 1: region members
-// 2: member functions to enter regions
-// 3: initializers
-// 4: args
-const char *const BIRGuardChildClass = R"(
-class BIR{0}Guard : BIRGuard {
-public:
-  BIR{0}Guard(mlir::OpBuilder &builder, {4})
-      : BIRGuard(builder), {3} {{}
-  ~BIR{0}Guard() = default;
-
-{2}
-
-private:
-{1}
-};
-)";
+std::vector<std::string> builderFunctionDecls;
 
 std::string getArgs(mlir::tblgen::Operator op) {
   std::vector<std::string> args;
@@ -67,77 +20,109 @@ std::string getArgs(mlir::tblgen::Operator op) {
   return llvm::join(args, ", ");
 }
 
-void genGuard(OpMetadata M) {
-  auto op = M.getOp();
-  llvm::StringRef name = op.getCppClassName();
-
+void emitGuardClassDecl(OpMetadata M, llvm::raw_ostream &os) {
   std::vector<std::string> regionNames = M.getRegionNames();
 
-  std::string dataMembers =
-      llvm::join(llvm::map_range(regionNames,
-                                 [&](const std::string &s) {
-                                   return "  mlir::Region *" + s + ";";
-                                 }),
-                 "\n");
+  os << "class " << M.getGuardName() << ": BIRGuard {\n";
+  os << "public:\n";
 
-  std::string memberFunctions =
-      llvm::join(llvm::map_range(regionNames,
-                                 [&](const std::string &s) {
-                                   return "  void enter_" + s + "();";
-                                 }),
-                 "\n");
+  // Constructor
+  os << "  " << M.getGuardName() << "(mlir::OpBuilder &builder\n";
+  for (auto r : regionNames)
+    os.indent(4) << ", mlir::Region *" << r << "\n";
+  os.indent(2) << ") ";
 
-  std::string inits = llvm::join(
-      llvm::map_range(regionNames,
-                      [&](const std::string &s) { return s + "(" + s + ")"; }),
-      ", ");
+  // Initializers
+  os << ": BIRGuard(builder)\n";
+  for (auto r : regionNames)
+    os.indent(4) << ", " << r << "(" << r << ")\n";
+  os.indent(2) << "{}\n";
 
-  std::string args = llvm::join(llvm::map_range(regionNames,
-                                                [&](const std::string &s) {
-                                                  return "mlir::Region *" + s;
-                                                }),
-                                ", ");
+  // Destructor
+  os.indent(2) << "~" << M.getGuardName() << "() = default;\n";
 
-  guards.push_back(llvm::formatv(BIRGuardChildClass, name, dataMembers,
-                                 memberFunctions, inits, args));
+  // Member Functions
+  for (auto r : regionNames)
+    os.indent(2) << "void enter_" << r << "();\n";
+
+  // Data members
+  os << "private:\n";
+  for (auto r : regionNames)
+    os.indent(2) << "mlir::Region *" << r << ";\n";
+
+  os << "};\n";
 }
 
-void genBuilderFunctionDecl(const llvm::Record *opRec, llvm::raw_ostream &os) {
-  mlir::tblgen::Operator op(opRec);
-  OpMetadata M(op);
-
-  if (!opRec->getValueAsBit("hasBIRGenBindings"))
-    return;
+void gatherBuilderFunctionDecls(OpMetadata M) {
+  auto op = M.getOp();
 
   std::string retTy =
       M.requiresGuard() ? "std::unique_ptr<" + M.getGuardName() + ">" : "void";
   auto args = getArgs(op);
 
   std::string fn = "  " + retTy + " " + M.getBuilderName() + "(" + args + ");";
-  builderFunctions.push_back(fn);
+  builderFunctionDecls.push_back(fn);
+}
 
+void emitAndGather(const llvm::Record *opRec, llvm::raw_ostream &os) {
+  if (!opRec->getValueAsBit("hasBIRGenBindings"))
+    return;
+
+  mlir::tblgen::Operator op(opRec);
+  OpMetadata M(op);
+
+  gatherBuilderFunctionDecls(M);
   if (M.requiresGuard())
-    genGuard(M);
+    emitGuardClassDecl(M, os);
+}
+
+void emitBIRGenClass(llvm::raw_ostream &os) {
+  os << "class BIRGen2 {\n"
+     << "public:\n"
+     << "  BIRGen2(::belalang::birgen::BIRGen &gen);\n"
+     << "  ~BIRGen2() = default;\n";
+
+  for (auto decl : builderFunctionDecls)
+    os << decl << "\n";
+
+  os << "private:\n"
+     << "  ::belalang::birgen::BIRGen &gen;\n"
+     << "};\n"
+     << "std::unique_ptr<BIRGen2> create_birgen2(uintptr_t gen);\n";
 }
 
 } // namespace
 
+static const char *const BIRGenForwardDecl = R"(
+namespace belalang {
+namespace birgen {
+class BIRGen;
+} // namespace birgen
+} // namespace belalang
+)";
+
+static const char *const BIRGuardBaseClass = R"(
+class BIRGuard {
+public:
+  BIRGuard(mlir::OpBuilder &builder) : guard(builder), builder(builder) {}
+  virtual ~BIRGuard() = default;
+
+protected:
+  mlir::OpBuilder::InsertionGuard guard;
+  mlir::OpBuilder &builder;
+};
+)";
+
 namespace belalang::bir {
 
 void emitCXXBindingDecls(const llvm::RecordKeeper &rk, llvm::raw_ostream &os) {
+  os << BIRGenForwardDecl;
+  os << "namespace belalang::birgen2 {\n";
+  os << BIRGuardBaseClass;
+
   for (const auto *op : rk.getAllDerivedDefinitions("BIR_Op"))
-    genBuilderFunctionDecl(op, os);
-
-  os << "namespace belalang {\n"
-     << "namespace birgen {\n"
-     << "class BIRGen;\n"
-     << "} // namespace birgen\n"
-     << "} // namespace belalang\n"
-     << "namespace belalang::birgen2 {\n";
-
-  os << BIRGuardClass << "\n";
-  os << llvm::join(guards, "\n");
-  os << llvm::formatv(BIRGenClass, llvm::join(builderFunctions, "\n"));
+    emitAndGather(op, os);
+  emitBIRGenClass(os);
 
   os << "} // namespace belalang::birgen2\n";
 }
