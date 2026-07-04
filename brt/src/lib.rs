d1f0d144009e20e9f803b297f5b76450fe8e7a80
Vito Secona <secona00@gmail.com>
2026-07-04 13:01:44 +0700

    brt: merge brt_core and brt

--- brt/src/lib.rs
+++ brt/src/lib.rs
@@ -8,6 +8,25 @@ unsafe extern "C" {
     fn GC_malloc(size: size_t) -> *mut c_void;
 }
 
+#[repr(C)]
+pub struct BrString {
+    pub ptr: *const u8,
+    pub len: u64,
+}
+
+impl BrString {
+    pub fn print(&self) {
+        unsafe {
+            let bytes = std::slice::from_raw_parts(self.ptr, self.len as usize);
+            if let Ok(s) = std::str::from_utf8(bytes) {
+                println!("{}", s);
+            } else {
+                eprintln!("invalid");
+            }
+        }
+    }
+}
+
 #[unsafe(no_mangle)]
 pub extern "C" fn brt_print_int(v: i64) {
     println!("{}", v)
@@ -19,7 +38,7 @@ pub extern "C" fn brt_print_float(v: f64) {
 }
 
 #[unsafe(no_mangle)]
-pub extern "C" fn brt_print_string(v: brt_core::string::BrString) {
+pub extern "C" fn brt_print_string(v: BrString) {
     v.print();
 }
 
