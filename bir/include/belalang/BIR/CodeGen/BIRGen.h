107991cdaab200abca4578ec572f6e2bba4474f1
Vito Secona <77039267+secona@users.noreply.github.com>
2026-07-07 18:24:10 +0700

    [bir][birgen][codegen] Move BIRGen C++ to bir_codegen (#280)
    This PR combines the old BIRGen implementation in `/birgen` new BIRGen
    implementation in `/bir` that is bir_codegen. This should make it
    simpler to add more to BIRGen without having two separate places. The
    differing BIRGen classes are also merged into one.
    
    Part of #272

--- bir/include/belalang/BIR/CodeGen/BIRGen.h
+++ bir/include/belalang/BIR/CodeGen/BIRGen.h
@@ -1,25 +1,75 @@
 #ifndef BELALANG_BIR_CODEGEN_BIRGEN_H_
 #define BELALANG_BIR_CODEGEN_BIRGEN_H_
 
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/Location.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/Target/LLVMIR/Dialect/LLVMIR/LLVMToLLVMIRTranslation.h"
+#include "mlir/Target/LLVMIR/Export.h"
+#include "rust/cxx.h"
+#include "llvm/IR/Module.h"
 #include <cstdint>
 #include <memory>
-#include "mlir/IR/MLIRContext.h"
-#include "mlir/IR/BuiltinOps.h"
-#include "mlir/IR/Builders.h"
-#include "mlir/IR/Location.h"
 
 namespace belalang {
-
-// forward declaration
-namespace birgen {
+namespace bir {
+namespace codegen {
+class LLVMGen;
+class BIRValue;
+class BIRGuard;
+class BIRFunctionGuard;
+class BIRIfGuard;
+class BIRWhileGuard;
+class BIRScopeGuard;
 class BIRGen;
-} // namespace birgen
 
+// NOTE: manual forward decl
+class BIRWhileOpGuard;
+} // namespace codegen
+} // namespace bir
+} // namespace belalang
+
+#include "birgen/src/lib.rs.h"
+
+namespace belalang {
 namespace bir {
 namespace codegen {
 
 // -----------------------------------------------------------------------------
-// BIRGuard
+// LLVMGen
+// -----------------------------------------------------------------------------
+
+std::unique_ptr<LLVMGen> create_llvmgen(BIRGen &gen);
+
+class LLVMGen {
+public:
+  LLVMGen(mlir::ModuleOp *module);
+  ~LLVMGen() = default;
+
+  rust::String dump_to_string() const;
+  rust::String compile_object_file(rust::String out) const;
+
+private:
+  llvm::LLVMContext context;
+  std::unique_ptr<llvm::Module> module;
+};
+
+// -----------------------------------------------------------------------------
+// BIRValue
+// -----------------------------------------------------------------------------
+
+class BIRValue {
+public:
+  BIRValue(mlir::Value value) : value(value) {}
+  mlir::Value getValue() const { return value; }
+
+private:
+  mlir::Value value;
+};
+
+// -----------------------------------------------------------------------------
+// BIRGuards
 // -----------------------------------------------------------------------------
 
 class BIRGuard {
@@ -35,22 +85,130 @@ protected:
 #define GET_BIRGUARD_CLASS_DECLS
 #include "belalang/BIR/CodeGen/Bindings.h.inc"
 
+class BIRFunctionGuard : public BIRGuard {
+public:
+  BIRFunctionGuard(mlir::OpBuilder &builder, mlir::Value fnValue,
+                   mlir::Region *bodyRegion)
+      : BIRGuard(builder), fnValue(fnValue), bodyRegion(bodyRegion) {}
+  ~BIRFunctionGuard() = default;
+
+  std::unique_ptr<BIRValue> get_value() const {
+    return std::make_unique<BIRValue>(fnValue);
+  }
+  std::unique_ptr<BIRValue> get_arg(size_t index) const;
+
+private:
+  mlir::Value fnValue;
+  mlir::Region *bodyRegion;
+};
+
+class BIRIfGuard : public BIRGuard {
+public:
+  BIRIfGuard(mlir::OpBuilder &builder, mlir::Region *thenRegion,
+             mlir::Region *elseRegion, mlir::Value resultValue)
+      : BIRGuard(builder), thenRegion(thenRegion), elseRegion(elseRegion),
+        resultValue(resultValue) {}
+  ~BIRIfGuard();
+
+  void start_then();
+  void start_else();
+  std::unique_ptr<BIRValue> get_value() const;
+
+private:
+  mlir::Region *thenRegion;
+  mlir::Region *elseRegion;
+  mlir::Value resultValue;
+};
+
+class BIRWhileGuard : public BIRGuard {
+public:
+  BIRWhileGuard(mlir::OpBuilder &builder, mlir::Region *condRegion,
+                mlir::Region *bodyRegion)
+      : BIRGuard(builder), condRegion(condRegion), bodyRegion(bodyRegion) {}
+  ~BIRWhileGuard() = default;
+
+  void start_cond();
+  void start_body();
+
+private:
+  mlir::Region *condRegion;
+  mlir::Region *bodyRegion;
+};
+
+class BIRScopeGuard : public BIRGuard {
+public:
+  BIRScopeGuard(mlir::OpBuilder &builder, mlir::Region *scopeRegion)
+      : BIRGuard(builder), scopeRegion(scopeRegion) {}
+  ~BIRScopeGuard() = default;
+
+  void start_body();
+
+private:
+  mlir::Region *scopeRegion;
+};
+
 // -----------------------------------------------------------------------------
 // BIRGen
 // -----------------------------------------------------------------------------
 
+std::unique_ptr<BIRGen> create_birgen();
+
 class BIRGen {
 public:
-  BIRGen(birgen::BIRGen &gen);
+  BIRGen();
+  ~BIRGen() = default;
+
+  std::unique_ptr<BIRValue> build_constant_int(int64_t val);
+  std::unique_ptr<BIRValue> build_constant_float(double val);
+  std::unique_ptr<BIRValue> build_constant_string(rust::Str val);
+  std::unique_ptr<BIRValue> build_constant_bool(bool val);
+
+  std::unique_ptr<BIRValue> build_binop(BinOpKind kind, const BIRValue &lhs,
+                                        const BIRValue &rhs);
+  std::unique_ptr<BIRValue> build_var_declare(const BIRValue &v,
+                                              rust::Str name);
+  std::unique_ptr<BIRValue> build_var_declare_ty(TypeKind v, rust::Str name);
+  std::unique_ptr<BIRValue> build_var_load(const BIRValue &refValue);
+  std::unique_ptr<BIRFunctionGuard>
+  build_fn_expr(TypeKind resultTy, rust::Slice<const TypeKind> paramTys);
+  std::unique_ptr<BIRIfGuard> build_if_expr(const BIRValue &cond);
+  std::unique_ptr<BIRWhileGuard> build_while_stmt();
+  std::unique_ptr<BIRScopeGuard> build_block_expr();
+  void build_condition(const BIRValue &cond);
+  void build_continue();
+  void build_break();
+  void build_var_store(const BIRValue &v, const BIRValue &ref);
+  void build_print(const BIRValue &val);
+  void build_return(const BIRValue &val);
+  void build_empty_return();
+  void build_main_return();
+  void build_yield(const BIRValue &val);
+  void build_empty_yield();
+
+  void start_call(const BIRValue &callee);
+  void add_call_arg(const BIRValue &arg);
+  std::unique_ptr<BIRValue> finish_call();
+
+  void dump() const;
+  rust::String dump_to_string() const;
+
+  bool optimize();
+
+  friend std::unique_ptr<LLVMGen> create_llvmgen(BIRGen &gen);
 
 #define GET_BUILDER_FUNCTION_DECLS
 #include "belalang/BIR/CodeGen/Bindings.h.inc"
 
 private:
-  birgen::BIRGen &gen;
-};
+  mlir::MLIRContext context;
+  mlir::ModuleOp module;
+  mlir::OpBuilder builder;
+  mlir::Location loc;
+  mlir::Value current_callee;
+  std::vector<mlir::Value> current_args;
 
-std::unique_ptr<BIRGen> create_birgen(uintptr_t gen);
+  mlir::Type mapType(TypeKind);
+};
 
 } // namespace codegen
 } // namespace bir
