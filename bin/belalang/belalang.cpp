a26c8a3337d14b2898c3e314ac1affb8f62ea473
Vito Secona <secona00@gmail.com>
2026-07-26 18:06:25 +0700

    [bin][belalang] Create belalang C++ binary (#309)
    This change implements the start of `belalang-cc` target, also known as
    belalang C++ binary. This is the start of belalang full C++ rewrite.

---
+++ bin/belalang/belalang.cpp
@@ -0,0 +1,48 @@
+#include "muopt/muopt.hpp"
+#include <iostream>
+#include <optional>
+#include <string_view>
+
+constexpr std::string_view help = R"(belalang
+
+Usage: belalang [OPTIONS] <COMMAND> <PATH>
+
+Commands:
+  build    Compile a .bel file
+  run      Run a .bel file
+  help     Print help message
+)";
+
+int main(int argc, char **argv) {
+  muopt::Parser parser(argc, argv);
+
+  std::optional<std::string> command;
+
+  auto arg = parser.next();
+  if (arg.has_value() && arg->is_plain())
+    command = arg->as_str();
+
+  if (!command.has_value()) {
+    std::cerr << help;
+    return 1;
+  }
+
+  if (*command == "help") {
+    std::cerr << help;
+    return 0;
+  }
+
+  if (*command == "build") {
+    std::cerr << "building\n";
+    return 0;
+  }
+
+  if (*command == "run") {
+    std::cerr << "running\n";
+    return 0;
+  }
+
+  std::cerr << "error: unknown command: " << *command << "\n";
+  std::cerr << "hint: available commands are build, run, help\n";
+  return 1;
+}
