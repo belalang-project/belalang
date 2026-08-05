9833d8b5aa24744a14905edbe00fa0218110fc8c
Vito Secona <secona00@gmail.com>
2026-08-06 01:21:12 +0700

    Remove Rust version to go full C++ (#318)
    Closes #313 
    
    - Remove the Rust implementation of Belalang in favor of C++ version
    - Moves BIR to the lib directory following other components
    - Replace belalang target with C++ version

--- tools/bir-tblgen/TableGenBackends.h
+++ tools/bir-tblgen/TableGenBackends.h
@@ -1,17 +1,6 @@
 #ifndef BIR_TOOLS_BIR_TBLGEN_TABLEGENBACKENDS_H_
 #define BIR_TOOLS_BIR_TBLGEN_TABLEGENBACKENDS_H_
 
-namespace llvm {
-class raw_ostream;
-class RecordKeeper;
-} // namespace llvm
-
-namespace belalang::bir {
-
-void emitRustBindingDecls(const llvm::RecordKeeper &, llvm::raw_ostream &);
-void emitCXXBindingDecls(const llvm::RecordKeeper &, llvm::raw_ostream &);
-void emitCXXBindingDefs(const llvm::RecordKeeper &, llvm::raw_ostream &);
-
-} // namespace belalang::bir
+namespace belalang::bir {} // namespace belalang::bir
 
 #endif // BIR_TOOLS_BIR_TBLGEN_TABLEGENBACKENDS_H_
