acdf9321d81b4619faeb686ea289fdc07c2da09b
Vito Secona <secona00@gmail.com>
2026-08-13 15:07:35 +0700

    [brt] Implement struct brt_obj

--- brt/src/gc.c
+++ brt/src/gc.c
@@ -2,15 +2,24 @@
 #include "errors.h"
 #include "utils.h"
 
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/mman.h>
 
+struct gc_obj {
+  size_t size;
+  bool marked;
+  struct gc_obj *next;
+  unsigned char data[];
+};
+
 struct gc_heap {
   uintptr_t hp;
   uintptr_t limit;
   uintptr_t space;
+  struct gc_obj *objects;
 };
 
 /// Global heap instance.
@@ -30,13 +39,19 @@ static void gc_heap_init(size_t size) {
 void brt_gc_init() { gc_heap_init(1024 * 1024); }
 
 void *brt_gc_alloc(size_t size) {
-  uintptr_t alloc_size = align_size(size);
+  uintptr_t payload_size = align_size(size);
+  uintptr_t alloc_size = sizeof(struct gc_obj) + payload_size;
 
   if (alloc_size > the_heap.limit - the_heap.hp)
     brt_fatal("out of GC heap memory");
 
-  void *result = (void *)the_heap.hp;
+  struct gc_obj *obj = (struct gc_obj *)the_heap.hp;
+  obj->size = payload_size;
+  obj->marked = false;
+  obj->next = the_heap.objects;
+  the_heap.objects = obj;
   the_heap.hp += alloc_size;
-  memset(result, 0, alloc_size);
-  return result;
+
+  memset(obj->data, 0, alloc_size);
+  return obj->data;
 }
