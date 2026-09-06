b1c34f41e02da3301b6858afb5ec1073b975ab47
Vito Secona <secona00@gmail.com>
2026-09-06 10:20:19 +0700

    [gcir] Add GCIRToLLVM conversion

--- lib/GCIR/IR/GCIRDialect.cpp
+++ lib/GCIR/IR/GCIRDialect.cpp
@@ -7,12 +7,20 @@
 #define GET_TYPEDEF_CLASSES
 #include "belalang/GCIR/IR/GCIRTypes.cpp.inc"
 
+#define GET_OP_CLASSES
+#include "belalang/GCIR/IR/GCIROps.cpp.inc"
+
 namespace belalang::gc {
 
 void GCIRDialect::initialize() {
   addTypes<
 #define GET_TYPEDEF_LIST
 #include "belalang/GCIR/IR/GCIRTypes.cpp.inc"
+      >();
+
+  addOperations<
+#define GET_OP_LIST
+#include "belalang/GCIR/IR/GCIROps.cpp.inc"
       >();
 }
 
